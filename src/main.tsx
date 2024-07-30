7e28e03bac23a23bbaa14ca9a13bf6a4a2e495cc
gabriel.bauer <gabriel.bauer@gft.com>
2024-07-29 22:45:02 -0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
-import { App } from './app'
 import { QuizzContextProvider } from './context/quizz-context'
 import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QuizzContextProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </QuizzContextProvider>
   </React.StrictMode>,
 )
